c538053dcec3cec57a0dd3721d43307c0e6e6ad4
jackie4u <jackie4u@seznam.cz>
2022-05-23 06:50:56 +0200

    Updating obsolete comments

--- DimePubWeb/Models/ViewModels/PodcastAdd.cs
+++ DimePubWeb/Models/ViewModels/PodcastAdd.cs
@@ -4,8 +4,6 @@ namespace DimePubWeb.Models.ViewModels
 {
     public class PodcastAdd
     {
-        // TODO: Re-implement with feed class?
-        // TODO: Check for length
         [Required(ErrorMessage = "Enter Source URL of the Feed")]
         [MaxLength(255)]
         public string? SourceUri { get; set; }
